1feda9e338fb01a0b2d07288eb25130c7304aee1
Søren Lykke Christensen <soer424c@edu.easj.dk>
2018-09-11 12:55:53 +0200

    Har tilføjet 3 tests, en for sort kaffe pris exceptions, en for Cortado pris exceptions og en for latte pris

--- KaffeNetCore.Test/UnitTest1.cs
+++ KaffeNetCore.Test/UnitTest1.cs
@@ -1,13 +1,53 @@
+using System;
+using kaffe;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KaffeNetCore;
 
 namespace KaffeNetCore.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class KaffeTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PrisLatteTest()
         {
+            // Arrange
+            Latte testLatte = new Latte();
+
+            // Act
+            var result = testLatte.Pris();
+
+            // Assert
+            Assert.AreEqual(40,result);
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SortKaffeRabatTest()
+        {
+            // Arrange
+            SortKaffe testSortKaffe = new SortKaffe(20);
+            
+            // Act
+            testSortKaffe.Pris();
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CortadoKaffeRabatTest()
+        {
+            // Arrange
+            Cortado testCortado = new Cortado(25);
+
+            // Act
+            testCortado.Pris();
+
+            // Assert
+            Assert.Fail();
         }
     }
 }
