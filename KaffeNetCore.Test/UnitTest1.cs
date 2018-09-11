57bb19586de63cc20414806a7146299e3a1c48af
Søren Lykke Christensen <soer424c@edu.easj.dk>
2018-09-11 12:30:38 +0200

    Tilføjet test project.

---
+++ KaffeNetCore.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KaffeNetCore.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
